e437b6ff1e82c97ae10fc44795f4594a55c83bf4
yuliiakodymska <kodymskayuliia@gmail.com>
2023-02-16 17:24:54 +0200

    fourth task

---
+++ fourthTask.py
@@ -0,0 +1,14 @@
+dict1 = {
+    "Armenia": "Yerevan",
+    "Ukraine": "Kyiv",
+    "Japan": "Tokyo",
+    "Iran": "Tehran",
+    "Austria": "Vienna",
+    "Germany": "Berlin",
+    "United States": "Washington DC"}
+
+sorted_dictionary = {}
+for i in sorted(dict1.keys()):
+    sorted_dictionary[i] = dict1[i]
+
+print(sorted_dictionary)
